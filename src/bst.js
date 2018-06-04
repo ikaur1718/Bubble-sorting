b57f49f5ee9a42b0b2bb838d9eebbc3a21b7e6a3
Rebecca M. Eom <mye391@gmail.com>
2018-06-04 17:44:29 -0400

    first commit

--- src/bst.js
+++ src/bst.js
@@ -1,1 +1,29 @@
-// YOUR CODE HERE
+class BinarySearchTree {
+  constructor(val) {
+    this.arr = [val];
+    this.left = null;
+    this.right = null;
+    this.value = val;
+  }
+
+  insert (num) {
+    this.arr.push(num);
+
+  }
+
+  contains () {
+
+  }
+
+  depthFirstForEach () {
+
+  }
+
+  breadthFirstForEach () {
+
+  }
+
+  size () {
+    return this.arr.length;
+  }
+}
